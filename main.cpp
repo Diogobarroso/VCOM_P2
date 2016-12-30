48d27f126c6be62b8072b96e7e4ee893df579dfe
Diogo Barroso <ei11105@fe.up.pt>
2016-12-30 14:49:53 +0000

    Merged functions to main, loaded labels

--- main.cpp
+++ main.cpp
@@ -1,10 +1,70 @@
-#include "utils.h"
 #include <opencv2\nonfree\features2d.hpp>
+#include <opencv2\opencv.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <fstream>
 
 using namespace std;
 using namespace cv;
 
+#define MIN_HESSIAN 400
+#define NUMBER_OF_FILES 500
+
 Mat trainingData, labels;
+
+vector< Mat > loadImages() {
+	vector<Mat> vec;
+	Mat img;
+
+	for (int i = 1; i <= 1000; i++)
+	{
+		img = imread("../train/" + to_string(i) + ".png", CV_LOAD_IMAGE_GRAYSCALE);
+		//flatten the image
+		//img.resize(1);
+		vec.push_back(img);
+	}
+	return vec;
+}
+
+void loadLabels() {
+	ifstream file("../labels/trainLabels.csv");
+	string line;
+
+	for (int i = 0; i < NUMBER_OF_FILES; i++) {
+		getline(file, line);
+		string label = line.substr(line.find(",") + 1);
+		if (label == "airplane")
+			labels.push_back(0);
+		else if (label == "automobile")
+			labels.push_back(1);
+		else if (label == "bird")
+			labels.push_back(2);
+		else if (label == "cat")
+			labels.push_back(3);
+		else if (label == "deer")
+			labels.push_back(4);
+		else if (label == "dog")
+			labels.push_back(5);
+		else if (label == "frog")
+			labels.push_back(6);
+		else if (label == "horse")
+			labels.push_back(7);
+		else if (label == "ship")
+			labels.push_back(8);
+		else if (label == "truck")
+			labels.push_back(9);
+	}
+
+}
+
+vector<KeyPoint> surf(Mat img) {
+	SurfFeatureDetector detector(MIN_HESSIAN);
+	vector<KeyPoint> kps;
+
+	detector.detect(img, kps);
+
+	return kps;
+}
 
 Mat sift(vector<Mat> images) {
 	Mat trainingDescriptors;
@@ -28,7 +88,7 @@ int main()
 {
 	vector<Mat> images = loadImages();
 
-	Mat trainingDescriptors = sift(images);
+	trainingData = sift(images);
 
 		
 	bool use_knn = 0;	
